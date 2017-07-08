325ae6801bf1dfd51a0ae3de30219987d1efda72
prodhan29 <02nahid02@gmail.com>
2017-07-09 00:59:45 +0600

    box added for section import

---
+++ src/FormBuilder/components/section/sectionImport.component.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class AddSection extends React.Component {
+
+    submit =(e)=>{
+        this.props.submit(e);
+        this.props.close();
+    }
+
+    toggleWindow=(e)=>{
+        e.stopPropagation(); 
+        this.props.close();
+    }
+    render() {
+        return (
+            <div id="sectionAdd" className="action_item" onClick = {this.toggleWindow}>Import Section
+                <div className="add_section_dropdown dropdown_panel">
+                    <input id="import-section" type="file" onChange={this.submit} />
+                </div>
+            </div>
+        );
+    }
+}
